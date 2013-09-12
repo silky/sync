e433c24c28677a781c03f862cd5a4e4a574af20a
Heather <heather@cynede.net>
2013-09-12 09:21:00 +0400

    cosmetics

--- __main__.py
+++ __main__.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
 #_____________________________________________________________________________________________
-'''
-              sync - Light sync util
-          Copyright (C)  2012-2013  Heather
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation; either
-version 3.0 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-'''
+''' Copyright (C)  2012-2013  Heather '''
 #_____________________________________________________________________________________________
 import os
 import string
@@ -225,9 +208,9 @@ def syncrepos(repos, recursive):
     for r in repos.split("\n"): 
         if r: SyncStarter(r, recursive)
 #_____________________________________________________________________________________________
-print("=====================================================================================")
-print("                     sync: Global repositories synchronizer v.2.7  ")
-print("=====================================================================================")
+print("======================================================================")
+print("         sync: Global repositories synchronizer v.2.7  ")
+print("======================================================================")
 #_____________________________________________________________________________________________
 config = ConfigParser()
 if os.name == 'nt':
@@ -245,9 +228,9 @@ else:
     syncrepos(root, False)
 #_____________________________________________________________________________________________
 print("  Statistics:  ")
-print("-------------------------------------------------------------------------------------")
+print("----------------------------------------------------------------------")
 print("      total : %d" % total)
 print("      success : %d" % success)
 print("      errors : %d" % error)
-print("=====================================================================================")
+print("======================================================================")
 #_____________________________________________________________________________________________
